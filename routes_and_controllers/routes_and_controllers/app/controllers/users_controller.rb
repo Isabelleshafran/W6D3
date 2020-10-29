49dd3cdeb405deb5caaa945a6f4b3acfcbf494e1
ishafran <isabelle.shafran@gmail.com>
2020-10-28 17:51:27 -0700

    W6D3

---
+++ routes_and_controllers/routes_and_controllers/app/controllers/users_controller.rb
@@ -0,0 +1,43 @@
+class UsersController < ApplicationController
+
+    def index
+        users = User.all
+        # render plain: "I'm in the index action!"
+        render json: users
+    end
+
+    def create
+        user = User.new(params.require(:user).permit(:name, :email))
+
+        if user.save
+            render json: user
+        else 
+            render json: user.errors.full_messages, status: :unprocessable_entity
+        end
+        
+    end
+
+    def show
+        user = User.find_by(id: params[:id])
+        render json: user
+    end
+
+    def update 
+        user = User.find(params[:id])
+        if user.update(params.require(:user).permit(:name, :email))
+            render json: user
+        else 
+            render json: user.errors.full_messages, status: :unprocessable_entity
+        end
+    end
+
+    def destroy 
+        user = User.find(params[:id])
+        if user.destroy
+            render json: user
+        else 
+            render json: "Can't destroy this user"
+        end
+    end
+
+end
